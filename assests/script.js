94c56616e31497195df44981686af61bb6b4481f
bsaglimb <bsaglimb>
2023-11-06 16:08:29 -0500

    added to the javascript

--- assests/script.js
+++ assests/script.js
@@ -81,9 +81,41 @@ function selectionAnswer(e){
     var isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     } else{
         selectedBtn.classList.add("incorrect");
     }
+    Array.from(answerButton.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
 };
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = 'You scored ${score} out of ${questions.length}!';
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+};
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+};
+
+nextButton.addEventListener("click", ()=>{
+    if (currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
 
 startQuiz();
